585763030e0d9c4e7c95b212db661f877a499b09
Nathan O'Hara <nathan.o.hara@duke.edu>
2020-10-24 15:34:33 -0400

    Allocating more memory to the cluster

--- whovotes_job.sh
+++ whovotes_job.sh
@@ -3,7 +3,7 @@
 #SBATCH -p common                                                               
 #SBATCH -N1                                                                     
 #SBATCH -c1                                                                     
-#SBATCH --mem=10G                                                               
+#SBATCH --mem=50G                                                               
 
 module load RStan/2.19.2
 Rscript working_model.R
