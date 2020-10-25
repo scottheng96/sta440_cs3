acefdd589ec941fa1091f5ae19b912242bea4c4b
Nathan O'Hara <nathan.o.hara@duke.edu>
2020-10-25 15:54:14 -0400

    fix plots code

--- whovotes_plots.R
+++ whovotes_plots.R
@@ -3,6 +3,6 @@ real_model = readRDS("whovotes_model_01var.rda.rds")
 param_names = parnames(real_model)[1:10]
 for (name in param_names){
   png(filename = paste("./plots/diagplot_", name), type = "cairo")
-  plot(real_model, pars = c(name))
+  plot(real_model, pars = c(name), ask = FALSE)
   dev.off()
 }
