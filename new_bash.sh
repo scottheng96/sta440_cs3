683f02a699bea7fb2c616f262acf218ad1e0b64b
Ashley Murray <42747966+ashleyjmurray@users.noreply.github.com>
2020-11-02 23:28:09 -0500

    Update new_bash.sh

--- new_bash.sh
+++ new_bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash                                                                     
-#SBATCH -- account=sta440-f20                                                   
+#SBATCH --account=sta440-f20                                                   
 #SBATCH -p common                                                               
 #SBATCH -N1                                                                     
 #SBATCH -c1                                                                     
