ae93f3b6858ccc86999a1c7dcae5a04c2f190194
Ashley Murray <42747966+ashleyjmurray@users.noreply.github.com>
2020-11-02 23:25:29 -0500

    Create new_bash.sh

---
+++ new_bash.sh
@@ -0,0 +1,11 @@
+#!/bin/bash                                                                     
+#SBATCH -- account=sta440-f20                                                   
+#SBATCH -p common                                                               
+#SBATCH -N1                                                                     
+#SBATCH -c1                                                                     
+#SBATCH --mem=50G                                                               
+#SBATCH --mail-type=end                                                         
+#SBATCH --mail-user=ajm120@duke.edu                                             
+
+module load R/3.6.0
+Rscript whovotes_bayesian_model_code.R
